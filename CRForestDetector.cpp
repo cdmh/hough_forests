85a6ef07dab2d73f7187426c3344041abc3cbb8b
Craig Henderson <hello@craighenderson.co.uk>
2015-09-22 17:59:24 +0100

    fixed voting problems

--- CRForestDetector.cpp
+++ CRForestDetector.cpp
@@ -55,21 +55,23 @@ CRForestDetector::accumulate_votes(
         cvGetRawData( imgDetect[c], (uchar**)&(ptDet[c]), &stepDet);
     stepDet /= sizeof(ptDet[0][0]);
 
-    int const xoffset = width/2;
-    int const yoffset = height/2;
+    int const startx = roi.x + width  / 2;
+    int const starty = roi.y + height / 2;
+    int const endx   = roi.x - width  / 2 + roi.width;
+    int const endy   = roi.y - height / 2 + roi.height;
 
     // data structure to record the leaf contributors to each pixel
     std::vector<std::vector<cv::Mat>> contributor_map(imgDetect.size());
 
     // cx,cy center of patch
 	vector<const LeafNode*> result(crForest_.GetSize());
-    for (int cy=yoffset+roi.y; cy<=yoffset+roi.y+roi.height-height; ++cy)
+    for (int cy=starty; cy<=endy; ++cy)
     {
         // Get start of row
         for (unsigned int c=0; c<features.size(); ++c)
             ptFCh_row[c] = &ptFCh[c][0];
 
-        for(int cx=xoffset+roi.x; cx<=roi.x+roi.width+xoffset-width; ++cx)
+        for(int cx=startx; cx<=endx; ++cx)
         {
             // regression for a single patch
             crForest_.regression(result, ptFCh_row, stepImg);
@@ -84,58 +86,48 @@ CRForestDetector::accumulate_votes(
                     // To speed up the voting, one can vote only for patches 
                     // with a probability for foreground > 0.5
                     // !!! CH This condition was commented out in the original code,
-                    //        with no indication why. It reduces processing from
-                    //        4m to 20s on a debug build, so worth having, and
-                    //        produces a good result. I haven't compared the
-                    //        accuracy fully yet, though
-                    if (leaf->pfg > 0.5)
+                    //        with no indication why. I haven't compared the
+                    //        accuracy fully yet
+                    if (leaf->pfg < 0.5)
+                        continue;
+
+                    // voting weight for leaf 
+                    float const w = leaf->pfg / float(leaf->vCenter.size() * result.size());
+
+                    // vote for all points stored in the leaf
+                    for (size_t ndx=0; ndx<leaf->vCenter.size(); ++ndx)
                     {
-                        // voting weight for leaf 
-                        float const w = leaf->pfg / float(leaf->vCenter.size() * result.size());
-
-                        // vote for all points stored in the leaf
-                        for (size_t ndx=0; ndx<leaf->vCenter.size(); ++ndx)
+                        if (inverted_forest_training)
                         {
-                            /// if we have trained the forest using the search dataset and the
-                            /// application is to find a smaller query image in that dataset,
-                            /// then we use a different voting mechanism
-                            int x, y;
-                            if (inverted_forest_training)
+                            size_t const frame = leaf->src_indices[ndx];
+                            for (size_t i=contrib.size(); i<=frame; ++i)
+                                contrib.push_back(cv::Mat::zeros(features[0]->height, features[0]->width, CV_32FC1));
+
+                            // we don't know the original image size, so we resize as we go
+                            // !!! this is inefficient, so could be fixed !!!
+                            int const mx = 1 + leaf->roi[ndx].x + leaf->roi[ndx].width;
+                            int const my = 1 + leaf->roi[ndx].y + leaf->roi[ndx].height;
+                            if (contrib[frame].cols < mx  ||  contrib[frame].rows < my)
                             {
-                                x = int(cx - (leaf->roi[ndx].width/2.) * ratios[c] + 0.5);
-                                y = int(cy - (leaf->roi[ndx].height/2.) + 0.5);
-                            }
-                            else
-                            {
-                                x = int(cx - leaf->vCenter[ndx][0].x * ratios[c] + 0.5);
-                                y = cy - leaf->vCenter[ndx][0].y;
+                                cv::Mat newimage = cv::Mat::zeros(std::max(my,contrib[frame].rows), std::max(mx,contrib[frame].cols), CV_32FC1);
+                                cv::Rect roi(cv::Point(0,0),contrib[frame].size());
+                                contrib[frame].copyTo(newimage(roi));
+                                swap(contrib[frame], newimage);
                             }
 
+                            // add the weight to the original patch location
+                            for (int cy1=leaf->roi[ndx].y; cy1<=leaf->roi[ndx].y+leaf->roi[ndx].height; ++cy1)
+                                for (int cx1=leaf->roi[ndx].x; cx1<=leaf->roi[ndx].x+leaf->roi[ndx].width; ++cx1)
+                                    contrib[frame].at<float>(cy1,cx1) += w;
+
+                                *(ptDet[c] + cx + cy*stepDet) += w;
+                        }
+                        else
+                        {
+                            int const x = int(cx - leaf->vCenter[ndx][0].x * ratios[c] + 0.5f);
+                            int const y = cy - leaf->vCenter[ndx][0].y;
                             if (x>=0  &&  y>=0  &&  x<imgDetect[c]->width  &&  y<imgDetect[c]->height)
-                            {
                                 *(ptDet[c] + x + y*stepDet) += w;
-
-                                size_t const frame = leaf->src_indices[ndx];
-                                for (size_t i=contrib.size(); i<=frame; ++i)
-                                    contrib.push_back(cv::Mat::zeros(features[0]->height,features[0]->width,CV_32FC1));
-//!!!!!!!!!!!!!!!!!!!!!!!!!
-// we don't know the original image size, so we'll resize as we go. this is very inefficient, so needs fixing
-                                {
-                                    auto const mx = 1+leaf->roi[ndx].x+leaf->roi[ndx].width;
-                                    auto const my = 1+leaf->roi[ndx].y+leaf->roi[ndx].height;
-                                    if (contrib[frame].cols < mx  ||  contrib[frame].rows < my)
-                                    {
-                                        cv::Mat newimage = cv::Mat::zeros(std::max(my,contrib[frame].rows), std::max(mx,contrib[frame].cols), CV_32FC1);
-                                        cv::Rect roi(cv::Point(0,0),contrib[frame].size());
-                                        contrib[frame].copyTo(newimage(roi));
-                                        swap(contrib[frame], newimage);
-                                    }
-                                }
-//!!!!!!!!!!!!!!!!!!!!!!!!!
-                                for (int cy1=leaf->roi[ndx].y; cy1<=leaf->roi[ndx].y+leaf->roi[ndx].height; ++cy1)
-                                    for (int cx1=leaf->roi[ndx].x; cx1<=leaf->roi[ndx].x+leaf->roi[ndx].width; ++cx1)
-                                        contrib[frame].at<float>(cy1,cx1) += w;
-                            }
                         }
                     }
                 } // end if
